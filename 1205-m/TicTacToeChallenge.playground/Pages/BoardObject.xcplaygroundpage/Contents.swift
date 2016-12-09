44355a0436c42576e66877d2aafc866b505480e0
Trevin Wisaksana <trevinwisaksana@gmail.com>
2016-12-09 12:20:05 -0800

    Partially working TicTacToe

--- 1205-m/TicTacToeChallenge.playground/Pages/BoardObject.xcplaygroundpage/Contents.swift
+++ 1205-m/TicTacToeChallenge.playground/Pages/BoardObject.xcplaygroundpage/Contents.swift
@@ -6,105 +6,95 @@ enum BoardState {
     case o, x
 }
 
+/* THINGS I'VE DONE:
+ – Winner method works
+ – Grid system works
+ –
+*/
+
+// Be advised, the logic I made is very confusing.
 class BoardObject {
-    // TODO: - Create current turn method
-        // Doesn't allow the same player to move twice 
-        // Sets the current turn
-    // TODO: - Create winner method
-        // Checks if an object has filled three boxes
-        // Places nil if there's no winner yet 
-        // Places draw if there's no winner
-    // TODO: - Create move method
-        // Takes an X and Y position
-        // Create an array which mimics the grid
-        // Assigns each part of the array to the grid
     
-    var currentTurn: BoardState!
-    var alreadyRuns = false
+    private var currentTurn: BoardState!
+    private var alreadyRuns = false
     
+    // Creating it's own grid to be independent from the user interface
     var grid: [[String]] = [["0", "0", "0"],
                             ["0", "0", "0"],
                             ["0", "0", "0"]]
     
-    func determineCurrentTurn() -> BoardState {
+    private func determineCurrentTurn() -> BoardState {
         switch currentTurn! {
         case .x:
-            print("Circle")
             return .o
         case .o:
-            print("Cross")
             return .x
         }
     }
     
-    func returnXorO(turn: BoardState) -> String {
+    private func returnXorO(turn: BoardState) -> String {
         switch turn {
         case .x: return "x"
         case .o: return "o"
         }
     }
     
-    func moveTo(x: Int, y: Int) {
+    public func moveTo(x: Int, y: Int) {
         if alreadyRuns == false {
             alreadyRuns = true
             currentTurn = .o
         }
-        
         let chosenTurn = determineCurrentTurn()
         let returnedState = returnXorO(turn: chosenTurn)
+        // ACHTUNG! X and Y are inverted here. This is because we are accessing nested arrays.
         grid[y][x] = returnedState
-        print(grid)
     }
     
-    func winner() {
-        if (grid[0][0].contains("x")) && (grid[0][0].contains("x")) && (grid[0][0].contains("x")) {
-            print("x")
-        } else if (grid[0][1].contains("x")) && (grid[1][1].contains("x")) && (grid[2][1].contains("x")) {
-            print("x")
-        } else if (grid[0][2].contains("x")) && (grid[1][2].contains("x")) && (grid[2][2].contains("x")) {
-            print("x")
-        } else if (grid[0][0].contains("x")) && (grid[0][1].contains("x")) && (grid[0][2].contains("x")) {
-            print("x")
-        } else if (grid[1][0].contains("x")) && (grid[1][1].contains("x")) && (grid[1][2].contains("x")) {
-            print("x")
-        } else if (grid[2][0].contains("x")) && (grid[2][1].contains("x")) && (grid[2][2].contains("x")) {
-            print("x")
-        } else if (grid[2][0].contains("x")) && (grid[1][1].contains("x")) && (grid[0][2].contains("x")) {
-            print("x")
-        } else if (grid[0][0].contains("x")) && (grid[1][1].contains("x")) && (grid[2][2].contains("x")) {
-            print("x")
-        }
+    /// Uses these conditions
+    private func conditions(XValue: Int, YValue: Int, symbol: String) -> Bool {
         
-        if (grid[0][0].contains("o")) && (grid[0][0].contains("o")) && (grid[0][0].contains("o")) {
-            print("o")
-        } else if (grid[0][1].contains("o")) && (grid[1][1].contains("o")) && (grid[2][1].contains("o")) {
-            print("o")
-        } else if (grid[0][2].contains("o")) && (grid[1][2].contains("o")) && (grid[2][2].contains("o")) {
-            print("o")
-        } else if (grid[0][0].contains("o")) && (grid[0][1].contains("o")) && (grid[0][2].contains("o")) {
-            print("o")
-        } else if (grid[1][0].contains("o")) && (grid[1][1].contains("o")) && (grid[1][2].contains("o")) {
-            print("o")
-        } else if (grid[2][0].contains("o")) && (grid[2][1].contains("o")) && (grid[2][2].contains("o")) {
-            print("o")
-        } else if (grid[2][0].contains("o")) && (grid[1][1].contains("o")) && (grid[0][2].contains("o")) {
-            print("o")
-        } else if (grid[0][0].contains("o")) && (grid[1][1].contains("o")) && (grid[2][2].contains("o")) {
-            print("o")
-        }
+        let firstCondition = (grid[XValue][0].contains(symbol)) && (grid[XValue][1].contains(symbol)) && (grid[XValue][2].contains(symbol))
         
+        let secondCondition = (grid[0][YValue].contains(symbol)) && (grid[1][YValue].contains(symbol)) && (grid[2][YValue].contains(symbol))
+        
+        let thirdCondition = (grid[0][YValue].contains(symbol)) && (grid[1][1].contains(symbol)) && (grid[2][XValue].contains(symbol))
+        
+        if firstCondition || secondCondition || thirdCondition {
+            return true
+        }
+        return false
+    }
+    
+    public func winner() -> String {
+        // Has to be -1 because it skips 0 if it doesn't start with -1
+        var XValue = -1
+        var YValue = -1
+        
+        // Loops 9 times as there are 9 total elements in the nested array
+        for i in 0...8 {
+            if (i % 3 != 0) {
+                XValue += 1
+            } else {
+                XValue = 0
+                YValue += 1
+            }
+            
+            if conditions(XValue: XValue, YValue: YValue, symbol: "x") == true {
+                return "Cross wins"
+            } else if conditions(XValue: XValue, YValue: YValue, symbol: "y") == true {
+                return "Circle wins"
+            } else {
+                return "There are no winners"
+            }
+        }
+        return "Loading..."
     }
 }
 
-// The logic is that when you call the moveto method, the
-
 let board = BoardObject()
 board.moveTo(x: 0, y: 0)
-board.moveTo(x: 1, y: 0)
-board.moveTo(x: 2, y: 0)
+board.moveTo(x: 0, y: 1)
+board.moveTo(x: 0, y: 2)
 board.winner()
-
-
-
 
 //: [Next](@next)
